88087fc2cd7f01496e0d8e775ecd3204b795a082
Ubuntu <ubuntu@ip-172-31-94-160.ec2.internal>
2022-08-12 14:27:06 +0000

    Added Kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ezeasorekene/alxproject:4.1_final
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flask-app --image=ezeasorekene/alxproject:4.1_final --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+sleep 100
+kubectl port-forward flask-app 8000:80
